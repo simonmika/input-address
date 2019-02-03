9c1b6c1f234a426047c59ba4eb057510f91fdfa8
Simon Mika <simon@mika.se>
2019-02-03 15:58:01 +0100

    Added support for prefixing fields.

--- src/components/input-address/input-address.tsx
+++ src/components/input-address/input-address.tsx
@@ -1,4 +1,4 @@
-import { Component } from "@stencil/core"
+import { Component, Prop } from "@stencil/core"
 
 @Component({
 	tag: "input-address",
@@ -6,14 +6,17 @@ import { Component } from "@stencil/core"
 	shadow: false,
 })
 export class InputAddress {
+	@Prop()
+	name: string
 	render() {
+		const prefix = this.name ? this.name + "_" : ""
 		return [
-			<label htmlFor="address">Adress</label>,
-			<input name="address" placeholder="Street 42" type="text"/>,
-			<label htmlFor="postCode">Post code</label>,
-			<input name="postCode" placeholder="133 70" type="text"/>,
-			<label htmlFor="city">City</label>,
-			<input name="city" placeholder="City" type="text"/>,
+			<label htmlFor={prefix + "street"}>Street Address</label>,
+			<input name={prefix + "street"} placeholder="Street 42" type="text"/>,
+			<label htmlFor={prefix + "postalCode"}>Post code</label>,
+			<input name={prefix + "postalCode"} placeholder="133 70" type="text"/>,
+			<label htmlFor={prefix + "postalTown"}>Postal Town</label>,
+			<input name={prefix + "postalTown"} placeholder="Postal Town" type="text"/>,
 		]
 	}
 }
