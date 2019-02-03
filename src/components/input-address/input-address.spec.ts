04eb628e05148deb69eec39f6e08b9f275ba4e97
Simon Mika <simon@mika.se>
2019-02-03 23:16:45 +0100

    Fixed issue running tests.

--- src/components/input-address/input-address.spec.ts
+++ src/components/input-address/input-address.spec.ts
@@ -9,24 +9,4 @@ describe("input-address", () => {
 		const element = await page.find("input-address")
 		expect(element).toHaveClass("hydrated")
 	})
-	it("renders changes to the name data", async () => {
-		const page = await newE2EPage()
-
-		await page.setContent("<input-address></input-address>")
-		const component = await page.find("input-address")
-		const element = await page.find("input-address >>> div")
-		expect(element.textContent).toEqual(`Hello, World! I"m `)
-
-		component.setProperty("first", "James")
-		await page.waitForChanges()
-		expect(element.textContent).toEqual(`Hello, World! I"m James`)
-
-		component.setProperty("last", "Quincy")
-		await page.waitForChanges()
-		expect(element.textContent).toEqual(`Hello, World! I"m James Quincy`)
-
-		component.setProperty("middle", "Earl")
-		await page.waitForChanges()
-		expect(element.textContent).toEqual(`Hello, World! I"m James Earl Quincy`)
-	})
 })
