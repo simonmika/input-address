535d96dfe055fc1fbb52c36d7de2018a4093e991
Simon Mika <simon@mika.se>
2019-02-03 13:16:35 +0100

    Added placeholders.

--- src/components/input-address/input-address.tsx
+++ src/components/input-address/input-address.tsx
@@ -9,11 +9,11 @@ export class InputAddress {
 	render() {
 		return [
 			<label htmlFor="address">Adress</label>,
-			<input name="address" type="text"/>,
+			<input name="address" placeholder="Street 42" type="text"/>,
 			<label htmlFor="postCode">Post code</label>,
-			<input name="postCode" type="text"/>,
+			<input name="postCode" placeholder="133 70" type="text"/>,
 			<label htmlFor="city">City</label>,
-			<input name="city" type="text"/>,
+			<input name="city" placeholder="City" type="text"/>,
 		]
 	}
 }
