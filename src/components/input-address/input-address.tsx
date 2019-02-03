e013c1c236b74fe2bf04038ead68f5809df52478
Simon Mika <simon@mika.se>
2019-02-03 14:42:02 +0100

    Removed shadow.

--- src/components/input-address/input-address.tsx
+++ src/components/input-address/input-address.tsx
@@ -3,7 +3,7 @@ import { Component } from "@stencil/core"
 @Component({
 	tag: "input-address",
 	styleUrl: "input-address.css",
-	shadow: true,
+	shadow: false,
 })
 export class InputAddress {
 	render() {
